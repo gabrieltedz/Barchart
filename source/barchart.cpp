13857d95ce274df219f405ef2516a6d7cb3ef3e8
Thiago_Raquel <thiaguinho2005@yahoo.com.br>
2023-09-26 19:35:24 -0300

    Arquivos iniciais

---
+++ source/barchart.cpp
@@ -0,0 +1,13 @@
+/*!
+ * Bar Char Race project.
+ *
+ * @author Selan
+ * @data October 5th, 2021.
+ */
+
+#include <string>
+using std::string;
+
+namespace bcra {
+    // TODO: Add class implementation here.
+}
